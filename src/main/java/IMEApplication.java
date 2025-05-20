26cc8291f1d2ac8b7b26280533c65e875ea7a1f9
sharan parikh <parikh.shar@northeastern.edu>
2025-05-19 23:41:10 -0400

    Updated main class to create the basic controller

--- src/main/java/IMEApplication.java
+++ src/main/java/IMEApplication.java
@@ -1,4 +1,22 @@
+import java.io.IOException;
+
+import controller.IMEController;
+import controller.IMEControllerBasic;
+import service.ImageProcessorBasicImpl;
+
 public class IMEApplication {
 
+  public static void main(String args[]) {
+    System.out.println("Welcome to IME application!");
+    IMEController controller = new IMEControllerBasic(System.in, System.out, new ImageProcessorBasicImpl());
+    try {
+      controller.go();
+    } catch (IOException e) {
+      System.out.println("An I/O error occurred!");
+    } catch (Exception e) {
+      System.out.println("An unexpected error occurred!");
+    }
+    System.out.println("Exiting...");
+  }
 
 }
